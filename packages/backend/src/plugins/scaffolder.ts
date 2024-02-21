966127f1e23d3dd3c5dfff4b88f8c57e6a5642f1
YuSongC <chuayusong@gmail.com>
2024-02-22 00:29:11 +0800

    Added templates

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,22 +1,31 @@
+import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
+import { ScmIntegrations } from '@backstage/integration';
+import { createNewFileAction } from './scaffolder/actions/custom';
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
-import type { PluginEnvironment } from '../types';
+import { PluginEnvironment } from '../types';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  const catalogClient = new CatalogClient({
-    discoveryApi: env.discovery,
+  const catalogClient = new CatalogClient({ discoveryApi: env.discovery });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
   });
 
-  return await createRouter({
+  const actions = [...builtInActions, createNewFileAction()];
+
+  return createRouter({
+    actions,
+    catalogClient: catalogClient,
     logger: env.logger,
     config: env.config,
     database: env.database,
     reader: env.reader,
-    catalogClient,
-    identity: env.identity,
-    permissions: env.permissions,
   });
 }
